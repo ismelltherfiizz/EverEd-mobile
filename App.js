49e5a50ceafa3a21f0452d830e781fdb34ebf7dc
Mykhailo Shkolnyi <shkolniy@dlit.dp.ua>
2019-05-05 23:35:06 +0300

    Transfered to expo and fixed dependencies troubles

--- App.js
+++ App.js
@@ -1,79 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
+import AppNavigator from './navigation/AppNavigator';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  };
 
-type Props = {};
-class Login exteds Component {
   render() {
-    return {
-      <View style={styles.container}>
-   <TabNavigator>
-     <TabNavigator.Item
-       title="Schedule"
-       renderIcon={() => <Image source={{uri: `${globals.serverUrl}/images/reactjs-conf/schedule-icon.png`}} style={styles.icon} />}
-       selected={this.state.selectedTab === 'schedule'}
-       onPress={() => { this.setState({selectedTab: 'schedule'}); }}>
-       <Navigator
-         style={[styles.container, styles.navigator]}
-         initialRoute={routes.list}
-         renderScene={this.renderScene.bind(this)}
-         navigationBar={NavBar}
-         ref={component => this._navigator = component} />
-     </TabNavigator.Item>
-     <TabNavigator.Item
-       title="Venue"
-       renderIcon={() => <Image source={{uri: `${globals.serverUrl}/images/reactjs-conf/venue-icon.png`}} style={styles.icon} />}
-       selected={this.state.selectedTab === 'venue'}
-       onPress={() => { this.setState({selectedTab: 'venue'}); }}>
-       <Screen title="Venue">
-         <Venue />
-       </Screen>
-     </TabNavigator.Item>
-     <TabNavigator.Item
-       title="Tweets"
-       renderIcon={() => <Image source={{uri: `${globals.serverUrl}/images/reactjs-conf/tweets-icon.png`}} style={styles.icon} />}
-       selected={this.state.selectedTab === ''}
-       onPress={() => { this.setState({selectedTab: ''}); }}>
-       <Screen title="1">
-         <Tweets />
-       </Screen>
-     </TabNavigator.Item>
-   </TabNavigator>
- </View>
-    };
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      );
+    }
   }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/robot-dev.png'),
+        require('./assets/images/robot-prod.png'),
+      ]),
+      Font.loadAsync({
+        // This is the font that we are using for our tab bar
+        ...Icon.Ionicons.font,
+        // We include SpaceMono because we use it in HomeScreen.js. Feel free
+        // to remove this if you are not using it in your app
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      }),
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
 }
-<View style={styles.container}>
-        <View style={styles.loginContainer}>
-                    <Image resizeMode="contain" style={styles.logo} source={require('../../components/images/logo.png')} />
-         </View>
 
-            <View style={styles.formContainer}>
-                   <LoginForm />
-            </View>
-       </View>
-
-// define your styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#2c3e50',
-    },
-    loginContainer:{
-        alignItems: 'center',
-        flexGrow: 1,
-        justifyContent: 'center'
-    },
-    logo: {
-        position: 'absolute',
-        width: 300,
-        height: 100
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
 });
